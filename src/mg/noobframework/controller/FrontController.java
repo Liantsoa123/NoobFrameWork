95436bfe49fe00391f44285566e4f10235cbbc80
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2024-05-14 22:30:09 +0300

    test function getcontroller

--- src/mg/noobframework/controller/FrontController.java
+++ src/mg/noobframework/controller/FrontController.java
@@ -2,17 +2,25 @@ package mg.noobframework.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import mg.noobframework.outils.ClassFinder;
 
 public class FrontController extends HttpServlet {
-    public void processRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    private ArrayList<Class<?>> listController;
+
+    public void processRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         PrintWriter out = resp.getWriter();
         out.println("<h1>Noob_FrameWork</h1>");
         out.println("<p>Votre url = " + req.getRequestURI() + "</p>");
+        out.println("huhu");
+        for (Class<?> class1 : listController) {
+            out.println("<p>" + class1.getSimpleName() + "</p>");
+        }
 
     }
 
@@ -25,5 +33,15 @@ public class FrontController extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         processRequest(req, resp);
+    }
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            String path = getServletContext().getRealPath("WEB-INF\\classes");
+            listController = ClassFinder.getController(path);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/mg/noobframework/outils/ClassFinder.java
+++ src/mg/noobframework/outils/ClassFinder.java
@@ -10,6 +10,7 @@ public class ClassFinder {
     public static ArrayList<Class<?>> getController(String packageName) throws ClassNotFoundException, IOException {
         ArrayList<Class<?>> classes = new ArrayList<>();
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
         String path = packageName.replace('.', '/');
 
         File packageDir = new File(classLoader.getResource(path).getFile().replace("%20", " "));
@@ -20,13 +21,25 @@ public class ClassFinder {
             } else {
 
                 String className = packageName + "." + file.getName().substring(0, file.getName().length() - 6);
-                // Controller controller =
                 Class<?> class1 = Class.forName(className);
+                // check if it's controller
                 if (class1.getDeclaredAnnotation(Controller.class) != null) {
                     classes.add(class1);
                 }
             }
         }
         return classes;
+    }
+
+    public static void main(String[] args) {
+        try {
+            ArrayList<Class<?>> list = getController("mg");
+
+            for (Class<?> class1 : list) {
+                System.out.println(class1.getSimpleName());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
