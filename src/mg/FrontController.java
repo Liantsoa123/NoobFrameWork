f8202ce638631ed637def497be9469640dd5919c
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2024-05-14 09:38:27 +0300

    sprint0 vita

---
+++ src/mg/FrontController.java
@@ -0,0 +1,29 @@
+package mg;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class FrontController extends HttpServlet {
+    public void processRequest(HttpServletRequest req, HttpServletResponse resp  ) throws IOException {
+        PrintWriter out = resp.getWriter();
+        out.println( "<h1>Noob_FrameWork</h1>" );
+        out.println("<p>Votre url = "+ req.getRequestURI() +"</p>");
+        
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+}
