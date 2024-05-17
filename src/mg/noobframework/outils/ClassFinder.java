3f672d1984170d3ee75a5ef7849d414674964029
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2024-05-17 11:18:01 +0300

    change the function getcontroller into getallclassannotation

--- src/mg/noobframework/outils/ClassFinder.java
+++ src/mg/noobframework/outils/ClassFinder.java
@@ -2,28 +2,27 @@ package mg.noobframework.outils;
 
 import java.io.File;
 import java.io.IOException;
+import java.lang.annotation.Annotation;
 import java.util.ArrayList;
 
-import mg.noobframework.annotation.Controller;
 
 public class ClassFinder {
-    public static ArrayList<Class<?>> getController(String packageName)
+    public static ArrayList<Class<?>> getAllClassAnnotation(String packageName , Class<? extends Annotation> annotation)
             throws ClassNotFoundException, IOException {
         ArrayList<Class<?>> classes = new ArrayList<>();
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         String path = packageName.replace('.', '/');
 
         File packageDir = new File(classLoader.getResource(path).getFile().replace("%20", " "));
-        System.out.println(packageDir.getAbsolutePath());
         for (File file : packageDir.listFiles()) {
             if (file.isDirectory()) {
-                classes.addAll(getController(packageName + "." + file.getName()));
+                classes.addAll(getAllClassAnnotation(packageName + "." + file.getName() , annotation ));
             } else {
 
                 String className = packageName + "." + file.getName().substring(0, file.getName().length() - 6);
                 Class<?> class1 = Class.forName(className);
                 // check if it's controller
-                if (class1.isAnnotationPresent(Controller.class)) {
+                if (class1.isAnnotationPresent(annotation)) {
                     classes.add(class1);
                 }
             }
