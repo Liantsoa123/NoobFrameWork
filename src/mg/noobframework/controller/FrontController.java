d2326975eb6e50ba013919ac19681dfa08dea6af
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2024-05-27 18:02:01 +0300

    finalize sprint2-2510

--- src/mg/noobframework/controller/FrontController.java
+++ src/mg/noobframework/controller/FrontController.java
@@ -2,26 +2,31 @@ package mg.noobframework.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import mg.noobframework.annotation.Controller;
+import mg.noobframework.outils.ClassFinder;
 import mg.noobframework.outils.Mapping;
 
 public class FrontController extends HttpServlet {
-    private HashMap < String , Mapping > listeMethodes ;
-    
-
-    
+    private HashMap<String, Mapping> listeMethodes;
 
     public void processRequest(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
         PrintWriter out = resp.getWriter();
+        String url = req.getRequestURI().replace("/NoobFramwork", "");
         out.println("<h1>Noob_FrameWork</h1>");
-        out.println("<p>Votre url = " + req.getRequestURI() + "</p>");
-
-
+        out.println("<p>Votre url = " + url + "</p>");
+        if (listeMethodes.get(url) != null) {
+            out.println("<p>Class= " + listeMethodes.get(url).getClassName() + "</p>");
+            out.println("<p>Method= " + listeMethodes.get(url).getMethodName() + "</p>");
+        } else {
+            out.println("There is no method associated with this path");
+        }
     }
 
     @Override
@@ -36,7 +41,13 @@ public class FrontController extends HttpServlet {
 
     @Override
     public void init() throws ServletException {
-        
+        String packageName = this.getInitParameter("controller_dir");
+        try {
+            ArrayList<Class<?>> controller = ClassFinder.getAllClassAnnotation(packageName, Controller.class);
+            listeMethodes = ClassFinder.getMethod(controller);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
