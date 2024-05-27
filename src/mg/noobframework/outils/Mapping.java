6d437020af173fc91959264ee74d5574e33430e7
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2024-05-27 16:28:21 +0300

    create class mapping (classname , methodName)

---
+++ src/mg/noobframework/outils/Mapping.java
@@ -0,0 +1,30 @@
+package mg.noobframework.outils;
+
+public class Mapping {
+    public Mapping() {
+    }
+
+    public Mapping(String className, String methodName) {
+        this.setClassName(className);
+        this.setMethodName(methodName);
+    }
+
+    private String className, methodName;
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+}
