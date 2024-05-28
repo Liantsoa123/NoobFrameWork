6bf5239aa17d0a4a849fc75a3a252cb3d64d91b2
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2024-05-28 10:59:11 +0300

    create function executMethod(mappign)

--- src/mg/noobframework/outils/MethodUtils.java
+++ src/mg/noobframework/outils/MethodUtils.java
@@ -1,6 +1,12 @@
 package mg.noobframework.outils;
 
+import java.lang.reflect.Method;
+
 public class MethodUtils {
-    public void executMethod( Mapping mapping ){
+    public Object executMethod(Mapping mapping) throws Exception {
+        Class<?> clazz = Class.forName(mapping.getClassName());
+        Object obj = clazz.getConstructor().newInstance();
+        Method method = clazz.getMethod(mapping.getMethodName());
+        return method.invoke(obj);
     }
 }
