4d5294e5535d70021712852bc798ab27ecd5d5f1
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2024-10-23 16:14:56 +0300

    change attribut filepath into fileName

--- src/mg/noobframework/file/File.java
+++ src/mg/noobframework/file/File.java
@@ -1,25 +1,37 @@
 package mg.noobframework.file;
 
+import jakarta.servlet.http.Part;
+
 public class File {
-    private byte[] bytes; 
-    private String filePath ;
+    private byte[] bytes;
+    private String fileName;
+
     public File() {
     }
-    public File(byte[] bytes, String filePath) {
+
+    public File(byte[] bytes, String fileName) {
         this.setBytes(bytes);
-        this.setFilePath(filePath);
+        this.setFileName(fileName);
     }
+
     public byte[] getBytes() {
         return bytes;
     }
+
     public void setBytes(byte[] bytes) {
         this.bytes = bytes;
     }
-    public String getFilePath() {
-        return filePath;
+
+    public String getFileName() {
+        return fileName;
     }
-    public void setFilePath(String filePath) {
-        this.filePath = filePath;
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void setByteFromPart(Part part) throws Exception {
+        this.setBytes(FileUtils.convertPartToBytes(part));
     }
 
 }
