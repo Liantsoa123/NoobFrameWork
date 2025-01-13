52163e6fe6d6b995edba2097198fc30d2e597998
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2025-01-13 11:25:27 +0300

    feat: add isAuth function.

--- src/mg/noobframework/auth/AuthMethod.java
+++ src/mg/noobframework/auth/AuthMethod.java
@@ -1,8 +1,14 @@
 package mg.noobframework.auth;
 
+import java.lang.reflect.Method;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mg.noobframework.annotation.Auth;
+
 public class AuthMethod {
     private String userName;
-    private String[] roles;
+    private String roles;
 
     public AuthMethod(String userName, String roles) {
         this.userName = userName;
@@ -20,12 +26,27 @@ public class AuthMethod {
         this.userName = userName;
     }
 
-    public String[] getRoles() {
+    public String getRoles() {
         return roles;
     }
 
     public void setRoles(String roles) {
-        this.roles = roles.split(",");
+        this.roles = roles;
     }
 
+    public boolean isAuth(HttpServletRequest request, Method method) {
+        String role_user = (String) request.getSession().getAttribute(roles);
+        boolean isAuth = false;
+        if (method.isAnnotationPresent(Auth.class)) {
+            Auth auth = method.getAnnotation(Auth.class);
+            String[] role = auth.value().split(",");
+            for (String r : role) {
+                if (role_user.equals(r)) {
+                    isAuth = true;
+                    break;
+                }
+            }
+        }
+        return isAuth;
+    }
 }
